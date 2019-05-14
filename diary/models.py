e606f476442511f187f4da2e6e5e7fca19a0d349
ddarling-ddarkling <ddarling.ddarkling@gmail.com>
2019-05-14 21:57:34 +0300

    First commit (model Diary)

---
+++ diary/models.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Diary(models.Model):
+    published_date = models.DateField(blank=True, null=True)
+    catch_date = models.DateField(blank=True, null=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    #fishing_time = models.TimeField()
+    #feeding_type = models.CharField(max_length=50)
+    #catch_type = models.CharField(max_length=50)
+    #tackle_type = models.CharField(max_length=50)
+    #weather = models.CharField(max_length=50)
+    #deleted = models.BooleanField() #??
