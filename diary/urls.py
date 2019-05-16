c5f111a4c66304e95d2f04527741900c875f3130
ddarling-ddarkling <ddarling.ddarkling@gmail.com>
2019-05-16 12:44:29 +0300

    Added forms for editing, adding and deleting

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.diary_page, name='diary_page'),
     path('diary/<int:pk>/', views.diary_detail, name='diary_detail'),
+    path('diary/new/', views.diary_new, name='diary_new'),
+    path('diary/<int:pk>/edit/', views.diary_edit, name='diary_edit'),
+    path('diary/<pk>/remove/', views.diary_remove, name='diary_remove'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Diary
 from django.shortcuts import render, get_object_or_404
+from .forms import DiaryForm
 
 
 def diary_page(request):
@@ -12,3 +13,36 @@ def diary_page(request):
 def diary_detail(request, pk):
     diary = get_object_or_404(Diary, pk=pk)
     return render(request, 'diary/diary_detail.html', {'diary': diary})
+
+
+def diary_new(request):
+    if request.method == "POST":
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            diary = form.save(commit=False)
+            diary.published_date = timezone.now()
+            diary.save()
+            return redirect('diary_detail', pk=diary.pk)
+    else:
+        form = DiaryForm()
+    return render(request, 'diary/diary_edit.html', {'form': form})
+
+
+def diary_edit(request, pk):
+    diary = get_object_or_404(Diary, pk=pk)
+    if request.method == "POST":
+        form = DiaryForm(request.POST, instance=diary)
+        if form.is_valid():
+            diary = form.save(commit=False)
+            diary.published_date = timezone.now()
+            diary.save()
+            return redirect('diary_detail', pk=diary.pk)
+    else:
+        form = DiaryForm(instance=diary)
+    return render(request, 'diary/diary_edit.html', {'form': form})
+
+
+def diary_remove(request, pk):
+    diary = get_object_or_404(Diary, pk=pk)
+    diary.delete()
+    return redirect('diary_page')
