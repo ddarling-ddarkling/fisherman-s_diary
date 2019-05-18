1a0224ea2dbfe54462f318f03b85b7799b6a1082
ddarling-ddarkling <ddarling.ddarkling@gmail.com>
2019-05-18 11:48:41 +0300

    Added sign up form

--- diary/forms.py
+++ diary/forms.py
@@ -6,4 +6,23 @@ class DiaryForm(forms.ModelForm):
 
     class Meta:
         model = Diary
-        fields = ('name', 'catch_date', 'fishing_time', 'weather', 'description', 'feeding_type', 'tackle_type', 'catch_type')
+        fields = (
+            'name',
+            'catch_date',
+            'fishing_time',
+            'weather',
+            'description',
+            'feeding_type',
+            'tackle_type',
+            'catch_type',
+        )
+        labels = {
+            'name': 'Название:',
+            'catch_date': 'Дата рыбалки:',
+            'fishing_time': 'Время суток:',
+            'weather': 'Погода:',
+            'description': 'Описание:',
+            'feeding_type': 'Тип прикорма:',
+            'tackle_type': 'Тип снасти:',
+            'catch_type': 'Тип улова:',
+        }
--- users/models.py
+++ users/models.py
@@ -1,4 +1,12 @@
-from django.db import models
-
-# Create your models here.
-# расширение users
+# from django.conf import settings
+# from django.db import models
+#
+#
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     region = models.CharField(max_length=160, null=True, blank=True)
+#     bio = models.CharField(max_length=160, null=True, blank=True)
+#     birthday = models.DateField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.user.username
